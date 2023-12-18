eb03cb4481332f9401290eb9c83502ea452cfbda
mic050r <103114387+mic050r@users.noreply.github.com>
2023-12-18 20:48:26 +0900

    feat : post를 이용하여 폼 주고 받기

--- src/main/java/com/example/noticeboard/controller/ArticleCotroller.java
+++ src/main/java/com/example/noticeboard/controller/ArticleCotroller.java
@@ -2,6 +2,7 @@ package com.example.noticeboard.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller // 컨트롤러 선언
 public class ArticleCotroller {
@@ -9,5 +10,10 @@ public class ArticleCotroller {
     @GetMapping("/articles/new") // URL 요청 접수
     public String newArticleForm() {
         return "articles/new";
+    }
+
+    @PostMapping("/articles/create")
+    public String createArticle() {
+        return "";
     }
 }
