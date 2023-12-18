ad613357f7f86479152d53d24953ea1f07f6bec5
mic050r <103114387+mic050r@users.noreply.github.com>
2023-12-18 21:18:50 +0900

    feat : dto를 엔티티로 변환

--- src/main/java/com/example/noticeboard/controller/ArticleCotroller.java
+++ src/main/java/com/example/noticeboard/controller/ArticleCotroller.java
@@ -1,6 +1,7 @@
 package com.example.noticeboard.controller;
 
 import com.example.noticeboard.dto.ArticleForm;
+import com.example.noticeboard.entity.Article;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,9 @@ public class ArticleCotroller {
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form) { // 폼 데이터를 DTO로 받기
         System.out.println(form.toString()); // DTO에 폼 데이터가 잘 담겼는지 확인
+        // 1. DTO를 엔티티로 변환
+        Article article = form.toEntity();
+        // 2. 리파지터리로 엔티티를 DB에 저장
         return "";
     }
 }
