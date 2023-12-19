8e388843a5f3f8e6bd496211a4ed5d03af30f451
mic050r <103114387+mic050r@users.noreply.github.com>
2023-12-19 17:34:42 +0900

    refactor : 롬복 추가하여 DTO 리팩토링

--- src/main/java/com/example/noticeboard/dto/ArticleForm.java
+++ src/main/java/com/example/noticeboard/dto/ArticleForm.java
@@ -1,25 +1,14 @@
 package com.example.noticeboard.dto;
 
 import com.example.noticeboard.entity.Article;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor // 새 어노테이션 추가 -> 생성자 삭제 ok
+@ToString // 새 어노테이션 추가 -> toString() 삭제 ok
 public class ArticleForm {
     private String title; // 제목을 받을 필드
     private String content; // 내용을 받을 필드
-
-    // 전송 받은 제목과 내용을 필드에 저장하는 생성자 추가
-    public ArticleForm(String title, String content) {
-        this.title = title;
-        this.content = content;
-    }
-
-    // 데이터를 잘 받았는지 확인할 toString() 메소드 추가
-    @Override
-    public String toString() {
-        return "ArticleForm{" +
-                "title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 
     public Article toEntity() {
         return new Article(null, title, content);
