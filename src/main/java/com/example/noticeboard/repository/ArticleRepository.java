b1dc8f4785cbc69af0d6cfaef0e45b6f9eb244d5
mic050r <103114387+mic050r@users.noreply.github.com>
2023-12-19 16:09:36 +0900

    feat : repository로 엔티티를 DB에 저장

---
+++ src/main/java/com/example/noticeboard/repository/ArticleRepository.java
@@ -0,0 +1,9 @@
+package com.example.noticeboard.repository;
+
+import com.example.noticeboard.entity.Article;
+import org.springframework.data.repository.CrudRepository;
+
+// Article : 관리 대상 엔티티의 클래스 타입, Long : 관리 대상 엔티티의 대푯값 타입
+public interface ArticleRepository extends CrudRepository<Article, Long> {
+
+}
