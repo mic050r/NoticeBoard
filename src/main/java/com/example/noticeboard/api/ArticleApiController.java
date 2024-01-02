433d894346139161c7e384d5fecbfcedbc9768cb
mic050r <103114387+mic050r@users.noreply.github.com>
2024-01-02 22:18:31 +0900

    refactor : 서비스 추가하여 게시글 삭제 요청 개선하기

--- src/main/java/com/example/noticeboard/api/ArticleApiController.java
+++ src/main/java/com/example/noticeboard/api/ArticleApiController.java
@@ -49,18 +49,13 @@ public class ArticleApiController {
                 ResponseEntity.status(HttpStatus.OK).body(updated) :
                 ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
-//
-//    // DELETE
-//    @DeleteMapping("/api/articles/{id}")
-//    public ResponseEntity<Article> delete(@PathVariable Long id){
-//        // 1. 대상 찾기
-//        Article target = articleRepository.findById(id).orElse(null);
-//        // 2. 잘못된 요청 처리하기
-//        if(target == null) {
-//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-//        }
-//        // 3. 대상 삭제하기
-//        articleRepository.delete(target);
-//        return ResponseEntity.status(HttpStatus.OK).build();
-//    }
+
+    // DELETE
+    @DeleteMapping("/api/articles/{id}")
+    public ResponseEntity<Article> delete(@PathVariable Long id){
+        Article deleted = articleService.delete(id);
+        return (deleted != null) ? // 생성하면 정상, 실패하면 오류 응답
+                ResponseEntity.status(HttpStatus.OK).build() :
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
 }
