d08ba092d08c669915235888b55c08889f0ebd71
mic050r <103114387+mic050r@users.noreply.github.com>
2024-01-02 20:44:01 +0900

    feat : RestAPI로 모든 게시글 조회하기

---
+++ src/main/java/com/example/noticeboard/api/ArticleApiController.java
@@ -0,0 +1,26 @@
+package com.example.noticeboard.api;
+
+import com.example.noticeboard.entity.Article;
+import com.example.noticeboard.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ArticleApiController {
+    @Autowired // 게시글 리파지터리 주입
+    private ArticleRepository articleRepository;
+
+    // GET
+    @GetMapping("/api/articles")
+    public List<Article> index(){
+        return articleRepository.findAll();
+    }
+
+
+    // POST
+    // PATCH
+    // DELETE
+}
