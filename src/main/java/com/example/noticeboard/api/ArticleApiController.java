61fbb01132e61ac2ca3b2ffe30dc596b634cac08
mic050r <103114387+mic050r@users.noreply.github.com>
2024-01-02 20:47:35 +0900

    feat : RestAPI로 단일 게시글 조회하기

--- src/main/java/com/example/noticeboard/api/ArticleApiController.java
+++ src/main/java/com/example/noticeboard/api/ArticleApiController.java
@@ -4,6 +4,7 @@ import com.example.noticeboard.entity.Article;
 import com.example.noticeboard.repository.ArticleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,6 +18,12 @@ public class ArticleApiController {
     @GetMapping("/api/articles")
     public List<Article> index(){
         return articleRepository.findAll();
+    }
+
+    // GET
+    @GetMapping("/api/articles/{id}")
+    public Article show(@PathVariable Long id){
+        return articleRepository.findById(id).orElse(null);
     }
 
 
